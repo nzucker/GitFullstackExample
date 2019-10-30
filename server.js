da7d2fcd07f8306f463684c2d6aebeecfd1cf749
nirnirle <nirnirle@gmail.com>
2019-10-30 06:38:58 +0200

    change port to PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 80;
+const port = process.env.PORT || 80;
 
 app.get('/test', (req, res) =>
     res.send("testing")
